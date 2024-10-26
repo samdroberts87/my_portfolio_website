d5558076fde3bc25ac6aaa31b6affa75bdc1cefb
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-26 14:42:41 +0100

    add working home buttons on child pages and add joke feature to webapp

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import random
 import string
+import requests
 
 app = Flask(__name__)
 
@@ -23,3 +24,24 @@ def password_generator(password_length):
     pool = string.ascii_letters + string.digits + string.punctuation
     selection = "".join(random.choice(pool) for i in range(password_length))
     return selection
+
+
+
+@app.route("/joke")
+def joke():
+    user_category = int(request.args["user_category"])
+    url = get_url(user_category)
+    final_joke = get_joke(url)
+    return render_template("joke.html", final_joke=final_joke)
+
+
+def get_url(number):
+    categories = {1: "Programming", 2: "Miscellaneous", 3: "Dark", 4: "Pun", 5: "Any"}
+    category = categories[number]
+    return f"https://v2.jokeapi.dev/joke/{category}?type=single"
+
+
+def get_joke(api_url):
+    response = requests.get(api_url)
+    data = response.json()
+    return data["joke"]
